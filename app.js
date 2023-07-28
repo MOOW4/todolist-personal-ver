d34a754c447b5ae0735f94a13f17725cbf91d99a
MOOW <christianfeliks1@gmail.com>
2023-07-28 14:43:21 +0200

    Almost there

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = 3000;
 
 //* VARS
 let tasks = [];
+let workTasks = [];
+
+//*
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,15 +18,29 @@ app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     let date = getDate();
-    res.render("index.ejs", {tasks: tasks, todayDate: date});
+    res.render("index.ejs", {tasks: tasks, Title: date});
 });
 
 app.post("/", (req, res) => {
     let task = req.body.taskText;
     if(task.length != 0){
-        tasks.push(task);
+        if(req.body.submitButton == "Work List"){
+            workTasks.push(task);
+            res.redirect("/work");
+        }
+        else{
+            tasks.push(task);
+            res.redirect("/");
+        }
     }
-    res.redirect("/");
+    else{    
+        res.redirect('back');
+    }
+});
+
+app.get("/work", (req, res) => {
+    let date = getDate();
+    res.render("index.ejs", {tasks: workTasks, Title: "Work List"});
 });
 
 app.listen(port, () => {
